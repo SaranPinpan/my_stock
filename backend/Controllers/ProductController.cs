ade840ba5cdfa5bc2d0c4dcd75f3b3b4d470f726
Saran Pinpan <5606021630217@fitm.kmutnb.ac.th>
2020-10-21 12:05:19 +0700

    init backend

---
+++ backend/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[products]")]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
