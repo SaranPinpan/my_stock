e476f8b11724027c27455953fcc2caf72a3fe32a
Saran Pinpan <5606021630217@fitm.kmutnb.ac.th>
2020-10-21 13:59:59 +0700

    Prepare installer + add db sqlite file

---
+++ backend/Installers/DBInstaller.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace backend.Installers
+{
+    public class DBInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            // services.AddDbContext<DataContext>(options =>
+            // options.UseSqlite(configuration.GetConnectionString("ConnectionSQLite")));
+
+            // services.AddDbContext<DataContext>(options =>
+            //   options.UseSqlServer(configuration.GetConnectionString("ConnectionSQLServer")));
+        }
+    }
+}
