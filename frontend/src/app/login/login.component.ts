f531ed9b476d634a9cc1a5f0a40a2722e0d99a46
Saran Pinpan <5606021630217@fitm.kmutnb.ac.th>
2020-10-22 13:14:08 +0700

    login frontend

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../models/user.model';
+import { NetworkService } from '../services/network.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("userForm") productForm: NgForm;
+
+  isShowLogin = true;
+  position = ["Admin", "Cashier"];
+
+  constructor(private networkService: NetworkService) { }
 
   ngOnInit(): void {
   }
 
+  async onSubmitLogin(userForm: NgForm) {
+    if (userForm.invalid) {
+      return;
+    }
+
+    let user: User = {
+      ...userForm.value
+    }
+
+    this.networkService.login(user).subscribe(
+      result => {
+        alert(result.token);
+      },
+      error => {
+        alert('Network Failure');
+      }
+    );
+  }
+
+  onClickShowLogin() {
+    this.isShowLogin = !this.isShowLogin;
+  }
+
+  async onSubmitRegister(userForm: NgForm) {
+    if (userForm.invalid) {
+      return;
+    }
+
+    let user: User = {
+      ...userForm.value
+    }
+
+    this.networkService.register(user).subscribe(
+      result => {
+        alert('Register successfuly');
+      },
+      error => {
+        alert('Network Failure');
+      }
+    );
+  }
 }
