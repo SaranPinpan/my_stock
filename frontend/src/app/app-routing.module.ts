c6789a2796143de3c19ffb327e9cc230025ed183
Saran Pinpan <5606021630217@fitm.kmutnb.ac.th>
2020-10-22 14:30:53 +0700

    add guard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,17 +2,18 @@ import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ReportComponent } from './report/report.component';
+import { AuthGuard } from './services/auth.guard';
 import { ShopComponent } from './shop/shop.component';
 import { StockFormComponent } from './stock-form/stock-form.component';
 import { StockComponent } from './stock/stock.component';
 
 const routes: Routes = [
-  { path: 'stock', component: StockComponent },
-  { path: 'stock/form', component: StockFormComponent },
-  { path: 'stock/form/:id', component: StockFormComponent },
-  { path: 'shop', component: ShopComponent },
-  { path: 'report', component: ReportComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'stock', component: StockComponent, canActivate: [AuthGuard] },
+  { path: 'stock/form', component: StockFormComponent, canActivate: [AuthGuard] },
+  { path: 'stock/form/:id', component: StockFormComponent, canActivate: [AuthGuard] },
+  { path: 'shop', component: ShopComponent, canActivate: [AuthGuard] },
+  { path: 'report', component: ReportComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'login' }
 ];
 
